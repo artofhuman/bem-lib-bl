b003388e7634915a84bc6ab96eb1c833d63aa5e4
Semen Pupkov <semen.pupkov@gmail.com>
2013-08-08 00:50:47 +0600

    fix slider js

--- b-slider/b-slider.js
+++ b-slider/b-slider.js
@@ -8,25 +8,27 @@ BEM.DOM.decl('b-slider', {
     onSetMod : {
 
         'js' : function() {
-            this._init();
-        },
-
-        '_init' : function() {
-            var wrap = this.findElem('wrap');
-
-            $(wrap).jcarousel({
-                list: '> ul',
-                items: '> li'
-            });
-        },
-
-        '_bindNavigation' : function() {
-            var prev = this.findElem('prev'),
-                next = this.findElem('next');
-
-                $(prev).jcarouselControl({'target': '-=1'});
-                $(next).jcarouselControl({'target': '+=1'});
+            this._initSlider();
+            this._bindNav();
         }
+
+    },
+
+    _initSlider : function() {
+        var wrap = this.findElem('wrap');
+
+        $(wrap).jcarousel({
+            list: '> ul',
+            items: '> li'
+        });
+    },
+
+    _bindNav : function() {
+        var prev = this.findElem('prev'),
+        next = this.findElem('next');
+
+        $(prev).jcarouselControl({'target': '-=1'});
+        $(next).jcarouselControl({'target': '+=1'});
     }
 
 });
